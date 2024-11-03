4a594174c0d7d9f8165b1e374cd5a3581aa276e7
Md Salauddin <salauddin1@uttarauniversity.edu.bd>
2024-11-03 21:36:10 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+public class Book{
+ 
+  public int Minus(int a, int b){
+     return a - b;
+   }
+
+   public double Minus(double a, double b){
+    return a - b;
+   }
+
+   public int Minus(int a, int b, int c){
+    return a - b - c;
+   }
+
+  public string name;
+  public int age;
+  public int roll;
+  public string phone;
+  
+
+  public Book(){
+    name = "Default";
+    age = 0;
+    roll = 0;
+    phone = "015**-******";
+  }
+
+  public Book(string Name, int Age, int Roll, string Phone){
+    name = Name;
+    age = Age;
+    roll = Roll;
+    phone = Phone;
+  }
+
+
+  public void Display(){
+
+    Console.WriteLine(" Personal Info ");
+    Console.WriteLine("Name : " + name);
+    Console.WriteLine("Age : " + age);
+    Console.WriteLine("Roll : " + roll);
+    Console.WriteLine("Phone : " + phone);
+    Console.WriteLine();
+
+  } 
+
+}
+
+public class LOki{
+    public static void Main(string[] args){
+    
+    Book man = new Book();
+    man.Display();
+    
+    Book man1 = new Book("Salauddin", 18, 1499, "01800-000000");
+    man1.Display();  
+
+    Book sum = new Book();
+    Console.WriteLine(sum.Minus(15, 9));
+    Console.WriteLine(sum.Minus(71, 18, 29));
+    Console.WriteLine(sum.Minus(59.34, 34.97));
+    Console.WriteLine();
+
+   }  
+
+}
